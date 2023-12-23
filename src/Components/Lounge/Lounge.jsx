71202152d82ce511fc1e6510e9587f57eb61a0fb
izalfront <rizalhajakubastarus@gmail.com>
2023-12-23 11:40:27 +0800

    lounge section

--- src/Components/Lounge/Lounge.jsx
+++ src/Components/Lounge/Lounge.jsx
@@ -1,7 +1,44 @@
 import React from 'react';
 
+//import assets ===>
+import imageGrid from '../../assets/grid2.png';
+
 const Lounge = () => {
-  return <div>Language</div>;
+  return (
+    <div className="lounge container section">
+      <div className="sectionContainer grid">
+        <div className="imgDiv">
+          <img src={imageGrid} alt="" />
+        </div>
+
+        <div className="textDiv">
+          <h2>Unaccompanied Minor Lounge</h2>
+
+          <div className="grids grid">
+            <div className="singleGrid">
+              <span className="gridTitle">Help throught the airpot</span>
+              <p>You can also call airlines from your phone and book a flight ticket to one of your favorite destinations.</p>
+            </div>
+
+            <div className="singleGrid">
+              <span className="gridTitle">Care on the flight</span>
+              <p>You can also call airlines from your phone and book a flight ticket to one of your favorite destinations.</p>
+            </div>
+
+            <div className="singleGrid">
+              <span className="gridTitle">Chauffeur-drive service</span>
+              <p>You can also call airlines from your phone and book a flight ticket to one of your favorite destinations.</p>
+            </div>
+
+            <div className="singleGrid">
+              <span className="gridTitle">Priority Boarding</span>
+              <p>You can also call airlines from your phone and book a flight ticket to one of your favorite destinations.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Lounge;
